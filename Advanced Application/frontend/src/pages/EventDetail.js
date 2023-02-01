ac700840338d8f51180252a3dea3807144dfbfa4
KSzczepa <87570436+KSzczepa@users.noreply.github.com>
2023-02-01 20:49:24 +0100

    Event details - cosmetic changes

--- Advanced Application/frontend/src/pages/EventDetail.js
+++ Advanced Application/frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { json, useRouteLoaderData } from "react-router-dom";
+import { json, redirect, useRouteLoaderData } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
@@ -9,18 +9,35 @@ function EventDetailPage() {
 }
 export default EventDetailPage;
 
-export async function loader({request, params}) {
+export async function loader({ request, params }) {
     const id = params.eventId;
 
-    const response = await fetch('http://localhost:8080/events/' + id); 
+    const response = await fetch('http://localhost:8080/events/' + id);
 
     if (!response.ok) {
-        throw json({message: 'Could not fetch details for selected event.'}, {
+        throw json({ message: 'Could not fetch details for selected event.' }, {
             status: 500
         })
     }
     else {
         return response;
     }
-    
+
+}
+
+
+export async function action({ params, request }) {
+    const eventId = params.eventId;
+    const response = await fetch('http://localhost:8080/events/' + eventId, {
+        method: request.method,
+    });
+
+    if (!response.ok) {
+        throw json(
+            { message: 'Could not delete event.' },
+            { status: 500 }
+        );
+    }
+
+    return redirect('/events');
 }
